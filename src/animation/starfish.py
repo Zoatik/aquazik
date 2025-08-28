0b65a8832365fd7eecd043e005d41012af4d40b9
Caro-13 <carolina71800@gmail.com>
2025-08-28 18:38:35 +0200

    Starfish body with random angle

--- src/animation/starfish.py
+++ src/animation/starfish.py
@@ -14,8 +14,25 @@ class Starfish:
         self.arm_count = 5
         self.arm_length = length
         self.arm_width = random.randrange(int(length/4),length)
+        self.angle = random.randrange(45)
 
         self.playing = True
+
+    def body(self):
+        pentagone = animation.drawings.getPolygonPoints(self.arm_count,self.center[0],self.center[1],self.arm_length/3)
+        pivotedPentagone = animation.drawings.pivotTriangles(self.center,pentagone,self.angle)
+        return pivotedPentagone
+    
+    def arms(self):
+        pentagone = self.body()
+        borderPoints = []
+        for i in range (len(pentagone)):
+            borderPoints.append((pentagone[i][1],pentagone[i][2]))
+        triangles = []
+        '''for border in borderPoints:
+            triangles.append(()) '''
+
+        return borderPoints
 
     # Function to create a single triangle for an arm
     def triangle_arm(self, center, length, width, angle):
@@ -32,17 +49,12 @@ class Starfish:
 
     def draw(self, borders: bool = False):
         self.color = Colors.patrick if self.playing else FishColors.orange
-        if not borders:
-            self.draw(borders=True)
-        for i in range(self.arm_count):
-            angle = i * (360 / self.arm_count) - 90  # rotate to start pointing upwards
-            points = self.triangle_arm(
-                self.center, self.arm_length, self.arm_width, angle
-            )
-            if borders:
-                pygame.draw.polygon(self.window, Colors.black, points, width=3)
-            else:
-                pygame.draw.polygon(self.window, self.color, points)
+
+        pentagone = self.body()
+        for triangle in pentagone:
+            pygame.draw.polygon(self.window,self.color,triangle)
+        
+        self.arms()
 
         if self.playing and self.arm_count == 5:
             self.drawPatrick()
